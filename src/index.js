46afdd788c6b2b926ef9decef441e10aac55a44a
Nadiia <malicenkonadic@gmail.com>
2023-03-06 16:57:02 +0200

    Add HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
@@ -9,13 +9,13 @@ import { BookProvider } from './context/BooksContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <BookProvider>
           <App />
         </BookProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
