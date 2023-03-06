3ef4775b7ac134a5c2ee892e95ca93100c8b5d8a
Nadiia <malicenkonadic@gmail.com>
2023-03-06 20:34:08 +0200

    Improve routing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
         <header>
                 {isAuthenticated ? ( 
                 <nav>
-                    <h1><Link to="/">JS BAND STORE /</Link><a href="https://endearing-taffy-335961.netlify.app/">Nadiia</a></h1>
+                    <h1><Link to="/signin">JS BAND STORE /</Link><a href="https://endearing-taffy-335961.netlify.app/">Nadiia</a></h1>
                     <Link to="/cart"><img src={defaultCartImage} id="cartImg" alt="Cart"/></Link>
                     <img onClick={logout} id="signBtn" alt="Log out" src="https://cdn.icon-icons.com/icons2/1524/PNG/512/signout_106525.png"/>
                     <p id="user">{userName}</p>
@@ -19,7 +19,7 @@ export default function Header() {
                 </nav>
                 ) : (
                 <nav>
-                    <h1><Link to="/">JS BAND STORE /</Link> <a href="https://endearing-taffy-335961.netlify.app/">Nadiia Malichenko</a></h1>
+                    <h1><Link to="/signin">JS BAND STORE /</Link> <a href="https://endearing-taffy-335961.netlify.app/">Nadiia Malichenko</a></h1>
                 </nav>
                 )}
         </header>
